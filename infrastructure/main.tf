cf02d45a844795a29ebc88c1bdd073240f893f16
nareknaka <grigoryan.narek.1993@gmail.com>
2025-06-12 15:31:59 +0400

    feat(terraform): add security group to allow SSH access from anywhere

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "webapp" {
   instance_type          = "t2.micro"
   key_name               = var.key_name
   associate_public_ip_address = true
+  vpc_security_group_ids      = ["sg-005cad66d86508e0a"]
 
   tags = {
     Name = "WebAppEC2"
