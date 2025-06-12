9223fcdacbf7880d6f367a72c0030c03a2b45e92
nareknaka <grigoryan.narek.1993@gmail.com>
2025-06-13 02:58:16 +0400

    Add EC2 instance + SG to Terraform config

---
+++ infrastructure/main.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "web_sg" {
+  name        = "webapp-sg"
+  description = "Allow SSH and app ports"
+  vpc_id      = "default" # թող տենց մնա եթե custom vpc չունես
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
