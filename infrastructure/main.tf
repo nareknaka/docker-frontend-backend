408ef4df51eb4ca909e24a146956de536307c389
nareknaka <grigoryan.narek.1993@gmail.com>
2025-06-13 05:13:22 +0400

    Add EC2 instance resource to provision

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,11 @@
-resource "aws_key_pair" "devops_key" {
-  key_name   = var.key_name
-  public_key = var.public_key_content
-}
+resource "aws_instance" "webapp" {
+  ami           = "ami-0c02fb55956c7d316" # Ubuntu 22.04 us-east-1
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.devops_key.key_name
 
+  vpc_security_group_ids = ["sg-005cad66d8508e0a"] # փոխիր ըստ քո group-ի ID-ի
+
+  tags = {
+    Name = "WebAppEC2"
+  }
+}
