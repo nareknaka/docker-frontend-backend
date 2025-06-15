14ce03a3a1d3328a3cdd8a8061a2ded0af3499d9
nareknaka <grigoryan.narek.1993@gmail.com>
2025-06-15 20:52:21 +0400

    test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,17 +1,18 @@
 provider "aws" {
-  region = var.region
+  region     = var.region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_key_pair" "devops_key" {
   key_name   = var.KEY_NAME
-  public_key = var.PUBLIC_KEY_CONTENT
+  public_key = var.PRIVATE_KEY_CONTENT
 }
 
 resource "aws_instance" "webapp" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.devops_key.key_name
-
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.devops_key.key_name
   vpc_security_group_ids = ["sg-005cad66d8508e0a"]
 
   tags = {
