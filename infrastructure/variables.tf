0b6e8bacc9401dc9faf5d8b2ffca44bce018dad2
nareknaka <grigoryan.narek.1993@gmail.com>
2025-06-15 20:04:03 +0400

    test

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -3,14 +3,14 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "key_name" {
+variable "KEY_NAME" {
   description = "Name of the key pair"
 }
 
-variable "public_key_content" {
+variable "PUBLIC_KEY_CONTENT" {
   description = "Public key content from GitHub secret"
 }
 
-variable "private_key_content" {
+variable "PRIVATE_KEY_CONTENT" {
   description = "Private key content from GitHub secret"
 }
