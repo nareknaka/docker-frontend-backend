9660ad72c1f10104294ff325fbb3c9020293ea7a
nareknaka <grigoryan.narek.1993@gmail.com>
2025-06-19 21:09:34 +0400

    test

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -7,14 +7,6 @@ variable "KEY_NAME" {
   description = "SSH key name"
 }
 
-variable "PUBLIC_KEY_CONTENT" {
-  description = "SSH public key content from GitHub secret"
-}
-
-variable "PRIVATE_KEY_CONTENT" {
-  description = "Private key content from GitHub secret"
-}
-
 variable "AWS_ACCESS_KEY_ID" {
   description = "AWS Access Key ID from GitHub secret"
 }
